523f3e8c87d44bdb07c857c5dd4f16c9c7723e78
Grigoriy Kutarev <gk@mactelabs.com>
2018-06-19 17:27:22 +0700

    make it really work

--- main.js
+++ main.js
@@ -4,27 +4,32 @@ var init = false;
 var on = false;
 var grid = {
 	size: 300,
-	start: 20,
-	end: 3300,
+	timeSpan: 600,
+	start: 100,
+	end: 5000,
 	sampleSize: 4096
 };
 var canvas = document.querySelector("canvas");
 var context = canvas.getContext("2d");
 
+var TEST = false;
+
 function buildGrid(grid) {
-	var minv = Math.log(grid.start);
-  	var maxv = Math.log(grid.end);
+	var minv = 12 * Math.log(grid.start / 440) / Math.log(2);
+  	var maxv = 12 * Math.log(grid.end / 440) / Math.log(2);
 	var length = maxv - minv;
 	grid.steps = new Float32Array(grid.size);
 	for(var i = 0; i < grid.size; i++) {
 		var t = i / grid.size;
-		t = Math.exp(minv + length * t);
+		t = 440 * Math.pow(2, (minv + t * length) / 12);
 		grid.steps[i] = t;
 	}
+	console.log(grid.steps);
 }
 
 function initCanvas() {
-	canvas.width = Math.min(600, window.innerWidth);
+	grid.timeSpan = Math.min(grid.timeSpan, window.innerWidth)
+	canvas.width = grid.timeSpan;
 	canvas.height = grid.size;
 
 	context.fillStyle = "#000";
@@ -37,13 +42,17 @@ function process(event) {
 	}
 	times++;
 
-	var inputBuffer = event.inputBuffer;
-	//if(times == 1) {
-		const data = event.inputBuffer.getChannelData(0);
-		worker.postMessage({ message: 'sound', data: data });
-		// data.sort((a, b) => a - b);
-		// console.log(data[0], data[data.length - 1]);
-	//}
+	if(TEST) {
+		var data = new Float32Array(4096);
+		for(let i=0; i<4096; i++) {
+			data[i] = Math.sin(2 * Math.PI * grid.steps[times * 10] * i / 44100) + Math.sin(2 * Math.PI * grid.steps[290] * i / 44100);
+		}
+	} else {
+		var inputBuffer = event.inputBuffer;
+		var data = event.inputBuffer.getChannelData(0);
+	}
+	
+	worker.postMessage({ message: 'sound', data: data });
 }
 
 buildGrid(grid);
@@ -56,8 +65,6 @@ worker.postMessage({ message: 'grid', data: grid });
 worker.onmessage = function(data) {
 	data = data.data;
 	addLine(data);
-	/*data.sort((a, b) => a - b);
-	console.log(data[0], data[data.length - 1]);*/
 }
 
 console.log(grid);
@@ -72,11 +79,12 @@ function addLine(histogram) {
 
 	var imgData = context.createImageData(1, grid.size);
 	var data = imgData.data;
+	var l = histogram.length - 1;
 	for(var i=0; i<histogram.length; i++) {
 		var j = i * 4;
-		data[j] = histogram[i] * histogram[i] * 255 | 0;
-		data[j + 1] = histogram[i] * 255 | 0;
-		data[j + 2] = Math.sqrt(histogram[i]) * 255 | 0;
+		data[j] = histogram[l - i] * histogram[l - i] * 255 | 0;
+		data[j + 1] = histogram[l - i] * 255 | 0;
+		data[j + 2] = Math.sqrt(histogram[l - i]) * 255 | 0;
 		data[j + 3] = 255;
 	}
 	context.putImageData(imgData, canvas.width - 1, 0);
@@ -93,23 +101,29 @@ function go1() {
 
 function start() {
 	if(!init) {
-		navigator.mediaDevices.getUserMedia({ video: false, audio: true })
-			.then(function(stream) {
-				var audioContext = new AudioContext();
-				var microphone = audioContext.createMediaStreamSource(stream);
-				window.source = microphone; // Workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=934512
-				var scriptProcessor = audioContext.createScriptProcessor(grid.sampleSize, 1, 1);
+		if(TEST) {
+			init = true;
+			on = true;
+			setInterval(process, 200);
+		} else {
+			navigator.mediaDevices.getUserMedia({ video: false, audio: true })
+				.then(function(stream) {
+					var audioContext = new AudioContext();
+					var microphone = audioContext.createMediaStreamSource(stream);
+					window.source = microphone; // Workaround for https://bugzilla.mozilla.org/show_bug.cgi?id=934512
+					var scriptProcessor = audioContext.createScriptProcessor(grid.sampleSize, 1, 1);
 
-				scriptProcessor.connect(audioContext.destination);
-				microphone.connect(scriptProcessor);
+					scriptProcessor.connect(audioContext.destination);
+					microphone.connect(scriptProcessor);
 
-				init = true;
-				on = true;
-				scriptProcessor.onaudioprocess = process;
-			})
-			.catch(function(err) {
-				console.log(err);
-			});
+					init = true;
+					on = true;
+					scriptProcessor.onaudioprocess = process;
+				})
+				.catch(function(err) {
+					console.log(err);
+				});
+		}		
 	} else {
 		on = true;
 	}
--- worker.js
+++ worker.js
@@ -1,12 +1,13 @@
 
 let grid;
+let threshold = 0.1;
 
 function buildGrid() {
 	grid.points = new Float32Array(grid.size * grid.sampleSize * 2);
 	for(let i=0; i<grid.size; i++) {
 		for(let j=0; j<grid.sampleSize; j++) {
 			let k = i * grid.sampleSize + j;
-			let a = j * Math.PI * grid.steps[i] / 44100;
+			let a = j * 2 * Math.PI * grid.steps[i] / 44100;
 			grid.points[k * 2] = Math.sin(a);
 			grid.points[k * 2 + 1] = Math.cos(a);
 		}
@@ -26,6 +27,10 @@ onmessage = function (message) {
 			let frequencies = fourier(message.data);
 			postMessage(frequencies);
 		break;
+
+		case 'threshold':
+			threshold = message.threshold;
+		break;
 	}
 }
 
@@ -33,6 +38,11 @@ function fourier(data) {
 	const output = new Float32Array(grid.size);
 	const points = grid.points;
 	let mean = 0;
+	let max = 0;
+	for(let i=0; i<data.length; i++) {
+		let t = i / data.length;
+		data[i] = data[i] * Math.sin(i * Math.PI / data.length);
+	}
 	for(let i = 0; i < grid.size; i++) {
 		let a = 0;
 		let b = 0;
@@ -41,14 +51,15 @@ function fourier(data) {
 			a += points[k] * data[j];
 			b += points[k + 1] * data[j];
 		}
-		a = Math.sqrt(a * a + b * b);
+		a = a * a + b * b;
+		max = a > max ? a : max;
 		output[i] = a;
 		mean += a;
 	}
 	mean /= grid.size;
-	mean += 1;
+	max = Math.max(max, threshold);
 	for(let i=0; i<output.length; i++) {
-		output[i] = output[i] * output[i] / (mean * mean);
+		output[i] = output[i] / max;
 	}
 	return output;
 }
